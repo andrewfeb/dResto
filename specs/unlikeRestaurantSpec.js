bd46c9617b478f55d12e5e0438473adf7353724c
Andrew Febrianto <andr3w.febrianto@gmail.com>
2021-06-27 06:27:21 +0700

    add integration test for unlike the restaurant

---
+++ specs/unlikeRestaurantSpec.js
@@ -0,0 +1,66 @@
+import likeButtonInitiator from '../src/scripts/utils/like-button-initiator';
+import favoriteRestaurantIdb from '../src/scripts/data/favorite-restaurants-db';
+
+describe('Unliking a Restaurant', () => {
+  const addLikeButton = () => {
+    document.body.innerHTML = '<button type="button" class="btn-like"></button>';
+  };
+
+  beforeEach(async () => {
+    addLikeButton();
+    await favoriteRestaurantIdb.put({ id: 1 });
+  });
+
+  afterEach(async () => {
+    await favoriteRestaurantIdb.delete(1);
+  });
+
+  it('should display unlike widget when the restaurant has been liked', async () => {
+    await likeButtonInitiator.init({
+      likeButton: document.querySelector('.btn-like'),
+      restaurant: {
+        id: 1,
+      },
+    });
+
+    expect(document.querySelector('[aria-label="Unlike this restaurant"]')).toBeTruthy();
+  });
+
+  it('should not display like widget when the restaurant has been liked', async () => {
+    await likeButtonInitiator.init({
+      likeButton: document.querySelector('.btn-like'),
+      restaurant: {
+        id: 1,
+      },
+    });
+
+    expect(document.querySelector('[aria-label="Like this restaurant"]')).toBeFalsy();
+  });
+
+  it('should be able to remove liked restaurant from the list', async () => {
+    await likeButtonInitiator.init({
+      likeButton: document.querySelector('.btn-like'),
+      restaurant: {
+        id: 1,
+      },
+    });
+
+    document.querySelector('[aria-label="Unlike this restaurant"]').dispatchEvent(new Event('click'));
+
+    expect(await favoriteRestaurantIdb.getAll()).toEqual([]);
+  });
+
+  it('should not throw error if the unliked restaurant is not in the list', async () => {
+    await likeButtonInitiator.init({
+      likeButton: document.querySelector('.btn-like'),
+      restaurant: {
+        id: 1,
+      },
+    });
+
+    await favoriteRestaurantIdb.delete(1);
+    document.querySelector('[aria-label="Unlike this restaurant"]').dispatchEvent(new Event('click'));
+
+    expect(await favoriteRestaurantIdb.getAll()).toEqual([]);
+  });
+});
