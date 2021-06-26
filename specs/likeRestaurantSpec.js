1f5d906bfe8c25758c7ea4aff8ca7d256f4e5fdf
Andrew Febrianto <andr3w.febrianto@gmail.com>
2021-06-27 06:04:29 +0700

    add integration test for like the restaurant

---
+++ specs/likeRestaurantSpec.js
@@ -0,0 +1,77 @@
+import likeButtonInitiator from '../src/scripts/utils/like-button-initiator';
+import favoriteRestaurantIdb from '../src/scripts/data/favorite-restaurants-db';
+
+describe('Like a Restaurant', () => {
+  const addLikeButton = () => {
+    document.body.innerHTML = '<button type="button" class="btn-like"></button>';
+  };
+
+  beforeEach(() => {
+    addLikeButton();
+  });
+
+  it('should show the like button when the restaurant has not been liked before', async () => {
+    await likeButtonInitiator.init({
+      likeButton: document.querySelector('.btn-like'),
+      restaurant: {
+        id: 1,
+      },
+    });
+
+    expect(document.querySelector('[aria-label="Like this restaurant"]')).toBeTruthy();
+  });
+
+  it('should not show the unlike button when the restaurant has not been liked before', async () => {
+    await likeButtonInitiator.init({
+      likeButton: document.querySelector('.btn-like'),
+      restaurant: {
+        id: 1,
+      },
+    });
+
+    expect(document.querySelector('[aria-label="Unlike this restaurant"]')).toBeFalsy();
+  });
+
+  it('should be able to like the restaurant', async () => {
+    await likeButtonInitiator.init({
+      likeButton: document.querySelector('.btn-like'),
+      restaurant: {
+        id: 1,
+      },
+    });
+
+    document.querySelector('.btn-like').dispatchEvent(new Event('click'));
+    const restaurant = await favoriteRestaurantIdb.get(1);
+
+    expect(restaurant).toEqual({ id: 1 });
+
+    favoriteRestaurantIdb.delete(1);
+  });
+
+  it('should not add a restaurant again when its already liked', async () => {
+    await likeButtonInitiator.init({
+      likeButton: document.querySelector('.btn-like'),
+      restaurant: {
+        id: 1,
+      },
+    });
+
+    await favoriteRestaurantIdb.put({ id: 1 });
+    document.querySelector('.btn-like').dispatchEvent(new Event('click'));
+
+    expect(await favoriteRestaurantIdb.getAll()).toEqual([{ id: 1 }]);
+
+    favoriteRestaurantIdb.delete(1);
+  });
+
+  xit('should not add a restaurant when it has no id', async () => {
+    await likeButtonInitiator.init({
+      likeButton: document.querySelector('.btn-like'),
+      restaurant: {},
+    });
+
+    document.querySelector('.btn-like').dispatchEvent(new Event('click'));
+
+    expect(await favoriteRestaurantIdb.getAll()).toEqual([]);
+  });
+});
