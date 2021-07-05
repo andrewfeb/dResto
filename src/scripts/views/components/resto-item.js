8e1cd881332ce43f56ba9b666bd0d126d20a845a
unknown <andr3w.febrianto@gmail.com>
2021-07-05 14:47:52 +0700

    to do image optimization

--- src/scripts/views/components/resto-item.js
+++ src/scripts/views/components/resto-item.js
@@ -8,7 +8,7 @@ class RestoItem extends HTMLElement {
 
   render() {
     this.innerHTML = `
-      <img src="${CONFIG.BASE_IMAGE_URL('small', this.restaurantItem.pictureId)}" alt="Image Restaurant ${this.restaurantItem.name}">
+      <img class="lazyload" data-src="${CONFIG.BASE_IMAGE_URL('small', this.restaurantItem.pictureId)}" alt="Image Restaurant ${this.restaurantItem.name}">
       <div class="card-body">
         <a href="/#/detail/${this.restaurantItem.id}"><h3 class="card-title">${this.restaurantItem.name}</h3></a>
         <div class="location"><i class="fas fa-map-marker-alt"></i>${this.restaurantItem.city}</div>
